412c2a1ca504345edc13a14b309332c9a2c78cfe
iamxiaoma <395487290@qq.com>
2018-10-04 02:13:53 +0800

    完善项目基础框架

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function (options) {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -11,6 +11,7 @@ App({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         console.log('login res', res);
+        this.globalData.code = res.code;
       }
     })
     // 获取用户信息
@@ -23,24 +24,83 @@ App({
             lang: 'zh_CN',
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              this.globalData.userInfo = res.userInfo;
               console.log('userInfo', res.userInfo);
+              var data = {
+                'code' : this.globalData.code,
+                'nickname': res.userInfo.nickName,
+                'headimgurl': res.userInfo.avatarUrl,
+                'sex': res.userInfo.gender,
+                'country': res.userInfo.country,
+                'province': res.userInfo.province,
+                'city': res.userInfo.city,
+              }
+              this.get(this.globalData.api.login, data).then((result) => {
+                console.log('result', result);
+                if(result.errcode == this.globalData.errcode.OK){
+                  if(result.data.openid.length > 0){
+                    this.globalData.openid = result.data.openid;
+                  }else{
+                    this.globalData.openid = '';
+                  }
+                  if(result.data.token.length > 0){
+                    this.globalData.token = result.data.token;
+                  }else{
+                    this.globalData.token = '';
+                  }
+                }else{
+                  wx.showToast({
+                    title: result.errmsg,
+                    icon: 'none',
+                    duration: 1500
+                  })
+                }
+              }).catch((err) => {
+                wx.showToast({
+                  title: err,
+                  icon: 'none',
+                  duration: 1500
+                })
+              })
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+              // if (this.userInfoReadyCallback) {
+              //   this.userInfoReadyCallback(res)
+              // }
             }
           })
         }
       }
     })
   },
+  onShow: function (options) {
+    // Do something when show.
+  },
+  onHide: function () {
+    // Do something when hide.
+  },
+  onError: function (msg) {
+    console.log(msg)
+  },
+  onPageNotFound: function (options){
+    
+  },
+  getUserInfo: function (e) {
+    console.log('app getUserInfo', e)
+    // 用户有可能点击拒绝授权
+    if (e.detail.userInfo) {
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+    }
+  },
   post: function(url, data){
     var that = this;
     var promise = new Promise((resolve, reject) => {
       wx.request({
-        url: url,
+        url: that.globalData.api_url + url,
         data: data,
         method: 'POST',
         header: {
@@ -65,7 +125,7 @@ App({
     var that = this;
     let promise = new Promise(function (resolve, reject) {
       wx.request({
-        url: url,
+        url: that.globalData.api_url + url,
         header: {
           'token': that.globalData.token,
           'content-type': 'application/json' // 默认值
@@ -75,7 +135,6 @@ App({
         success: function (res) {
           if (res.data.errcode == 200) {
             resolve(res.data);
-
           } else {//返回错误提示信息
             reject(res.data.errmsg);
             //reject('请求出错,请稍后重试');
@@ -90,7 +149,16 @@ App({
   },
   globalData: {
     userInfo: null,
+    code: '',
     token: '',
-    api_url: 'http://api.tvaf.local.cn/weapp'
+    openid: '',
+    api_url: 'http://api.tvaf.local.cn/weapp/',
+    api: {
+      login: 'login',
+    },
+    errcode: {
+      OK: 200,
+      FAIL: 500,
+    }
   }
 })
--- pages/member/member.js
+++ pages/member/member.js
@@ -1,4 +1,4 @@
-//index.js
+//member.js
 //获取应用实例
 const app = getApp()
 
@@ -9,7 +9,7 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -20,7 +20,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -45,10 +45,10 @@ Page({
   onTabItemTap(item) {
     console.log('onTabItemTap', item)
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log('getUserInfo', e)
     // 用户有可能点击拒绝授权
-    if(e.detail.userInfo){
+    if (e.detail.userInfo) {
       app.globalData.userInfo = e.detail.userInfo
       this.setData({
         userInfo: e.detail.userInfo,
